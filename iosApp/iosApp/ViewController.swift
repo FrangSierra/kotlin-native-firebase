5716fab549d62548b84277c3e81bc23bb65698d9
Jorge Revuelta Herrero <jorge.revuelta@bq.com>
2019-10-02 12:37:00 +0200

    Something happens

--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -1,10 +1,26 @@
 import UIKit
 import app
+import FirebaseAuth
+import FirebaseCore
+import FirebaseAnalytics
+import FirebaseInstanceID
+import FirebaseCoreDiagnostics
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = "asdasd \(Sample().checkMe())"
+        Auth().createUserWithEmailAndPassword(email: "minuscorp@gmail.com",
+                                              password: "password",
+                                              onComplete: { (user: FirebaseUser?) -> KotlinUnit in
+                                                guard let user = user else { return KotlinUnit() }
+                                                print(user.id)
+                                                return KotlinUnit()
+        },
+                                              onError: { (error: KotlinException?) -> KotlinUnit in
+                                                guard let error = error else { return KotlinUnit() }
+                                                print(error.description)
+                                                return KotlinUnit()
+        })
     }
 
     override func didReceiveMemoryWarning() {
